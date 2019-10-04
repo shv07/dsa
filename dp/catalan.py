949a9cf4bca7e4df1487b10362d04185f5924864
shv07 <shivji.bhagat@iitgn.ac.in>
2019-10-04 13:04:39 +0530

    added catalan no.

---
+++ dp/catalan.py
@@ -0,0 +1,23 @@
+'''
+A program to find the nth catalan no.
+Nth Catalan No. refers to the no. of distinct bst that can be formed with n nodes.
+'''
+
+data = {}
+def catalan(n):
+    if n<=1:
+        return 1
+    if n in data:
+        return data[n]
+    cat = 0
+    for i in range(1,n+1):
+        cat+=catalan(i-1)*catalan(n-i)
+    data[n]=cat
+    return cat
+
+ans = []
+for _ in range(int(input().strip())):
+    n = int(input().strip())
+    ans.append(catalan(n))
+for i in ans:
+    print(i)
